7618e6461fd63c37bffb40188b90047e3bee8720
AnionB <90142322+AnionB@users.noreply.github.com>
2022-04-26 16:01:46 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
-import Statistics from './Statistics';
-import FeedbackOptions from './FeedbackOptions';
-import Notification from './Notification';
-import Section from './Section';
+import Statistics from './Statistics/Statistics';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Notification from './Notification/Notification';
+import Section from './Section/Section';
 import './app.css';
 
 export class App extends Component {
